3bf484a370fc015e4aaa11c129912d3ead8f6031
Digvijay Rathore <rathore.digvijay10@gmail.com>
2020-04-26 21:21:49 +0530

    changes in about page and help page

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.',
+        helpText: 'This is some helpful text for now.',
         title: 'Help',
         name: 'Digvijay Rathore'
     })
